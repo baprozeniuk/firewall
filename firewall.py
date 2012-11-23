b08473d49987dcbd4020661c584b1d3188a2f885
Brandt Prozeniuk <brandt.prozeniuk@gmail.com>
2012-11-23 14:32:20 -0500

    added main subroutines

--- firewall.py
+++ firewall.py
@@ -32,7 +32,14 @@ iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o $WANINTERFACE -j SNAT --to-sourc
 iptables -I POSTROUTING -t nat -o $WANINTERFACE -d $WANADDRESS/24 -j MASQUERADE
 """
 ###################################################################
-def GenNat(WANAddr,LANNet,WANInt,LANInt):
+def GenNat(WANAddr,WANMask,LANNet,LANMask,WANInt,LANInt):
+	nat = "sysctl -w net.ipv4.ip_forward=1\n"
+	nat += "iptables -P FORWARD ACCEPT\n"
+	nat += "iptables -F FORWARD\n"
+	nat += "iptables -A FORWARD -i " + WANInt + " -o " + LANInt + " -m state --state ESTABLISHED,RELATED -j ACCEPT\n"
+	nat += "iptables -A FORWARD -i " + LANInt + " -o " + WANInt + " -j ACCEPT\n"
+	nat += "iptables -t nat -A POSTROUTING -s " + LANNet + "/" + LANMask + " -o " + WANInt + " -j SNAT --to-source " + WANAddr + "\n"
+	nat += "iptables -I POSTROUTING -t nat -o " + WANInt + " -d " + WANAddr + "/" + WANMask + " -j MASQUERADE\n"
 	return nat
 
 ###################################################################
@@ -49,6 +56,8 @@ iptables -I FORWARD -p tcp -d 10.60.0.3 --dport 45678 -j ACCEPT
 ##
 ###################################################################
 def GenPortMap(extPort,intPort,intIP,proto):
+	map = "iptables -t nat -I PREROUTING -p " + proto + " --dport " + extPort + " -j DNAT --to " + intIP + ":" + intPort +"\n"
+	map += "iptables -I FORWARD -p " + proto + " -d " + intIP + " --dport " + intPort + " -j ACCEPT\n" 
 	return map
 ###################################################################
 ##                                                               
@@ -64,6 +73,7 @@ iptables -I INPUT -i eth0 -p udp --dport 161 -j ACCEPT
 ##
 ###################################################################.
 def GenOpenPort(port,proto,extInt):
+	port = "iptables -I INPUT -i " + extInt + " -p " + proto + " --dport " + port + " + -j ACCEPT\n"
 	return port
 ###################################################################
 ##                                                               
@@ -79,6 +89,7 @@ route add -net 10.9.9.0/24 gw 10.66.12.1
 ##
 ###################################################################
 def GenStaticNetRoute(net,mask,gw):
+	route = "route add -net " + net + "/" + mask + " gw " + gw + "\n"
 	return route
 
 ###################################################################
@@ -92,8 +103,14 @@ def GenStaticNetRoute(net,mask,gw):
 ##
 ###################################################################
 def main():
-	print "place holder"
-
+	print "#nat"
+	print GenNat("95.211.129.156","32","10.0.0.0","8","eth0","eth1")
+	print "#Port map"
+	print GenPortMap("4500","4500","10.9.9.50","tcp")
+	print "#OpenPort"
+	print GenOpenPort("5900","tcp","eth0")
+	print "#Static Route"
+	print GenStaticNetRoute("10.9.9.0","24","10.66.12.1")
 #start	
 if __name__ == "__main__":
     main()
